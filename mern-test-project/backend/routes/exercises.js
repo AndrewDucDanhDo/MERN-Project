059552d8723a92266de5e91adb55b7147d523342
AndrewDDDo <37526974+AndrewDDDo@users.noreply.github.com>
2020-06-24 02:19:26 +1000

    commit on 'master'

--- mern-test-project/backend/routes/exercises.js
+++ mern-test-project/backend/routes/exercises.js
@@ -46,7 +46,7 @@ router.route('/update/:id').post((req,res) => {
             exercise.date = Date.parse(req.body.date);
 
             exercise.save()
-                .then(() => res.json('Exercise update!'))
+                .then(() => res.json('Exercise updated!'))
                 .catch(err => res.status(400).json('Error: ' + err));
         })
         .catch(err => res.status(400).json('Error: ' + err));
